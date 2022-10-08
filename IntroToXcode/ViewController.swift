4554704a7299e2d7d4a7e9f27bcd991a436b46f1
anarraidoo <32473241+anarraidoo@users.noreply.github.com>
2022-10-08 09:15:46 -0400

    First Xcode project. A simple iOS app with swift that allows you to submit your name. Project implemented during Cheyenne Williams's Sego Lily Hacks '22 workshop

--- IntroToXcode/ViewController.swift
+++ IntroToXcode/ViewController.swift
@@ -14,6 +14,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    //for our first app, we want to be able to populate a label with our name. Use double slashes to comment what each object below is and what purpose it will serve in our app. write the description ABOVE the object
 
+    //write the description for the object in line 4 here: label
+    @IBOutlet weak var label: UILabel!
+
+    // text field that will hold the user's typing
+    @IBOutlet weak var textField: UITextField!
+
+    // button that will do something when pressed
+    @IBAction func buttonPressed(sender: Any){
+      // when the button is pressed, if the user wrote something in a est field, what they wrote will show up in a label
+      if let userName = textField.text{
+        label.text = userName
+      }
+    }
 }
 
